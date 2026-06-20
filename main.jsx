22461b73ed620faf1809492b08816de0ff7b4bfe
shazwansuhiman <31242316+shazwansuhiman@users.noreply.github.com>
2026-06-20 12:38:13 +0800

    Initialize main.jsx with React and UmrahRPG component

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import UmrahRPG from './umrah-simulator.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <UmrahRPG />
+  </React.StrictMode>,
+)
